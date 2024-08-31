c7233dbd22ee9ac22f3af6a57b8438d1374a81c1
andrey <A.Ovchinnikov@aol.com>
2024-08-31 19:21:53 +0400

    liquibase start

---
+++ src/test/java/ru/me/springliquibasestart/SpringLiquibaseStartApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.me.springliquibasestart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLiquibaseStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
